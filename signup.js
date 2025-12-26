77e89c0c414e1afc10772bb235e033c7dcbbc86a
SIMBA <mikeraelly015@gmail.com>
2025-12-26 17:47:50 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -7,13 +7,14 @@ img1.addEventListener("click", () => {
     const isPassword = box1.type === 'password';
     box1.type = isPassword ? 'text' : 'password';
 
-    img1.src = isPassword ? 'visibility-on.png': 'images/visibility-off.png'
+    img1.src = isPassword ? 'visibility-on.png': 'visibility-off.png'
 })
 
 img2.addEventListener("click", () => {
     const isPassword = box2.type === 'password';
     box2.type = isPassword ? 'text' : 'password';
 
-    img2.src = isPassword ? 'visibility-on.png': 'images/visibility-off.png'
+    img2.src = isPassword ? 'visibility-on.png': 'visibility-off.png'
 
 })
+
