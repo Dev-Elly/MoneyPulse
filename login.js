b02fd1bb7aa94027a4f609de92b811a8d7b14ce2
SIMBA <mikeraelly015@gmail.com>
2025-12-26 17:21:24 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+const box1 = document.getElementById('password')
+const img = document.getElementById('togglePassword');
+
+img.addEventListener("click", () => {
+    const isPassword = box1.type === 'password';
+    box1.type = isPassword ? 'text' : 'password';
+
+    img.src = isPassword ? 'images/visibility-on.png': 'images/visibility-off.png'
+})
---
+++ signup.js
@@ -0,0 +1,18 @@
+const box1 = document.getElementById('password1')
+const box2 = document.getElementById('password2')
+const img1 = document.getElementById('togglePassword1');
+const img2 = document.getElementById('togglePassword2');
+
+img1.addEventListener("click", () => {
+    const isPassword = box1.type === 'password';
+    box1.type = isPassword ? 'text' : 'password';
+
+    img1.src = isPassword ? 'images/visibility-on.png': 'images/visibility-off.png'
+})
+
+img2.addEventListener("click", () => {
+    const isPassword = box2.type === 'password';
+    box2.type = isPassword ? 'text' : 'password';
+
+    img2.src = isPassword ? 'images/visibility-on.png': 'images/visibility-off.png'
+})
